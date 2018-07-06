117c98830d9c59c0ffc50cf816e0cc108fc961fe
Simon Chae <s13chae@gmail.com>
2018-07-06 09:44:18 -0700

    added tags.js

---
+++ routes/tags.js
@@ -0,0 +1,85 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const knex = require('../knex');
+
+router.get('/tags', (req, res, next) => {
+  knex.select('id', 'name')
+    .from('tags')
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => next(err));
+});
+
+router.get('/tags/:id', (req, res, next) => {
+  const id = req.params.id;
+  knex('tags')
+    .first('id', 'name')
+    .where('id', `${id}`)
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => next(err));
+});
+
+router.put('/tags/:id', (req, res, next) => {
+  const id = req.params.id;
+  const updateObj = {};
+  const updateableFields = ['name'];
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      updateObj[field] = req.body[field];
+    }
+  });
+  if (!updateObj.title) {
+    const err = new Error('Missing `name` in request body');
+    err.status = 400;
+    return next(err);
+  }
+  knex('tags')
+    .where('id', `${id}`)
+    .update(`${updateObj.name}`)
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+router.post('/tags', (req, res, next) => {
+  const {name} = req.body;
+  if (!name) {
+    const err = new Error('Missing `name` in request body');
+    err.status = 400;
+    return next(err);
+  }
+  const newItem = { name };
+  knex.insert(newItem)
+    .into('tags')
+    .returning(['id', 'name'])
+    .then((results) => {
+      // Uses Array index solution to get first item in results array
+      const result = results[0];
+      res.location(`${req.originalUrl}/${result.id}`).status(201).json(result);
+    })
+    .catch(err => next(err));
+});
+
+router.delete('/tags/:id', (req, res, next) => {
+  const id = req.params.id;
+  knex('tags')
+    .where('id', `${id}`)
+    .del()
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+
+module.exports = router;
